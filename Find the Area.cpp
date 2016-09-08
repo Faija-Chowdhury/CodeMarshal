d675206d2315d3d6a9f8c6799b42b54bbfc45077
shanta <evilevil4991@yahoo.com>
2016-09-09 00:03:32 +0600

    Create Find the Area.cpp

---
+++ Find the Area.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<cstdio>
+#include<math.h>
+using namespace std;
+#define M_PI acos(-1.0)
+int main() {
+int t;
+	double c,d,p,l,r;
+	cin>>t;
+	while(t--){
+	    cin>>p;
+	    r = p/2;
+	    l = 0.5 * 2 * p *p;
+	    c = M_PI *r*r;
+	    d= l-c;
+	    printf("%0.2lf\n",d);
+	    
+	    
+	}
+	return 0;
+}
